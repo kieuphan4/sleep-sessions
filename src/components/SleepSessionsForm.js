71bcebdeb7058040644d5f56896510bdf7e7c66b
kieuphan4 <kieuphan530@gmail.com>
2022-02-23 23:34:00 -0600

    added file structures to components

---
+++ src/components/SleepSessionsForm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SleepSessionsForm() {
+  return (
+    <div>SleepSessionsForm</div>
+  )
+}
+
+export default SleepSessionsForm
---
+++ src/components/SleepSessionsList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SleepSessionsList() {
+  return (
+    <div>SleepSessionsList</div>
+  )
+}
+
+export default SleepSessionsList
