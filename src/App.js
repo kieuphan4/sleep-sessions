780d0605ccf1d82e703b0ebd11b922c0e3d136c3
kieuphan4 <kieuphan530@gmail.com>
2022-02-23 23:42:30 -0600

    imported components to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home'
+import Signup from './components/Signup'
+import Login from './components/Login'
+import SleepSessionsList from './components/SleepSessionsList'
+import SleepSessionsForm from './components/SleepSessionsForm'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Home />
+      <Signup />
+      <Login />
+      <SleepSessionsList />
+      <SleepSessionsForm />
     </div>
   );
 }
